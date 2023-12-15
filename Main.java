fa3bf360d07b9c3059bdc4e24864b9e45fc08c1d
rahulmallula <152957987+rahulmallula@users.noreply.github.com>
2023-12-15 12:12:00 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -13,9 +13,9 @@ public class Main {
     }
 
     if (!flag)
-      System.out.println(num + " is a prime number.");
+      System.out.println(num + " is a primeee.");
     else
-      System.out.println(num + " is not a prime number.");
+      System.out.println(num + " is not a primeeee.");
   }
 }
 
